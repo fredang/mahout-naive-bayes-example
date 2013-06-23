82c0075e09cfebf8e142ff34297c5eefdff295b7
IronMonkey <frederic.dangngoc@gmail.com>
2013-06-23 18:12:51 -0400

    updated twitter_fetcher.py to use twitter v1.1 with the tweepy library

--- scripts/twitter_fetcher.py
+++ scripts/twitter_fetcher.py
@@ -14,23 +14,31 @@
 # limitations under the License.
 #
 
-import urllib2
-import json
+import tweepy
 import sys
+
+CONSUMER_KEY='REPLACE_CONSUMER_KEY'
+CONSUMER_SECRET='REPLACE_CONSUMER_SECRET'
+ACCESS_TOKEN_KEY='REPLACE_ACCESS_TOKEN_KEY'
+ACCESS_TOKEN_SECRET='REPLACE_ACCESS_TOKEN_SECRET'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 pageCount = 5
 if len(sys.argv) >= 2:
 	pageCount = int(sys.argv[1])
 hashtags = ['deal', 'deals', 'discount']
 
-for i in range(1, pageCount + 1):
-	for tag in hashtags:
-		url = "http://search.twitter.com/search.json?q=%%23%s&rpp=100&result_type=recent&page=%d" % (tag, i)
-		req = urllib2.Request(url)
-		res = urllib2.urlopen(req)
-		content = res.read()
-		for result in json.loads(content)['results']:
+for tag in hashtags:
+	maxId = 999999999999999999999
+	for i in range(1, pageCount + 1):
+		results = api.search(q='#%s' % tag, max_id=maxId, count=100)
+		print len(results)
+		for result in results:
+			print result.text
+			maxId = min(maxId, result.id)
 			# only keep tweets pointing to a web page
-			if result['text'].find("http:") != -1:
-				print "%s	%s" % (result['id'], result['text'].encode('utf-8').replace('\n', ' '))
-	
+			if result.text.find("http:") != -1:
+				print "%s	%s" % (result.id, result.text.encode('utf-8').replace('\n', ' '))
